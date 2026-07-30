06dc8954021b4bb412f68a84d3639f9a588777ab
Rohan-Basnet <rohbas05@gmail.com>
2026-07-30 21:25:41 +0545

    started customer user registration

--- tweet/forms.py
+++ tweet/forms.py
@@ -1,10 +1,13 @@
 from django import forms 
 from .models import Tweet
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields =["text","image"]
         
-    
+class UserRegistrationForm(UserCreationForm):
+    email=forms.EmailField()
+    class Meta:
+        fields=( 'username','email','password1','password2')
