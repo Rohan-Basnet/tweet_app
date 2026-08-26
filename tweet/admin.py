8f579536b7e406fdfc3dafe302ab418997fd0608
Rohan-Basnet <rohbas05@gmail.com>
2026-08-26 22:00:10 +0545

    add follow functionality

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tweet,Profile,Comments,Like
+from .models import Tweet,Profile,Comments,Like,Follow
 
 
 # Register your models here.
@@ -7,3 +7,4 @@ admin.site.register(Tweet)
 admin.site.register(Profile)
 admin.site.register(Comments)
 admin.site.register(Like)
+admin .site.register(Follow)
